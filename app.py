1aa0e1bd49ec716c8c4e68282b8ede3ca57c29e1
CesarMCuellarCha <cesarmcuellar@gmail.com>
2025-03-15 09:41:13 -0500

    Actualización archivo app.py. Creación cadena de conexión local y remota

--- app.py
+++ app.py
@@ -9,7 +9,13 @@ app.secret_key = '3209139823928139'
 #configurar carpeta donde se van a guardar las fotos de los productos
 app.config["UPLOAD_FOLDER"]="./static/imagenes/"
 
-miConexion = MongoClient("mongodb://localhost:27017/")
+#uri conexión local
+uri="mongodb://localhost:27017/"
+#uri remota (aquí cada uno obtiene de acuerdo a su base de datos remota)
+#remplazan <db_username> Y <db_password> en la cadena obtenida
+uri="mongodb+srv://<db_username>:<db_password>@runt.oudoapr.mongodb.net/?retryWrites=true&w=majority&appName=RUNT"
+miConexion = MongoClient(uri)
+
 
 #crear base datos
 baseDatos = miConexion["Negocio"]
